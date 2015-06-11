d5c9784a44d13aba76c5ac410fb142dabfbcfee2
info206 <info206@LAB02-14.edupit.intra>
2015-06-11 16:28:59 -0300

    PARTE PALOMA 2

--- src/Controller/FilmeController.java
+++ src/Controller/FilmeController.java
@@ -4,10 +4,44 @@
  */
 package Controller;
 
+import Model.Filme;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author info206
  */
 public class FilmeController {
+     public void Cadastrar(Filme f) throws SQLException {
+        try {
+
+            Util util = new Util();
+             try (Connection conexao = util.conecta()) {
+                 String sql = "INSERT INTO filme (Nome,Categoria,Codigo,Quantidade,d) VALUES (?, ?, ?, ?, ?)";
+                 PreparedStatement statement = conexao.prepareStatement(sql);// note que agora criamos um Statement de forma diferente
+                 statement.setString(1, f.getNome());
+                 statement.setString(2, f.getCategoria());
+                 statement.setString(3, f.getCodigo());
+                 statement.setString(4, f.getQuantidade());
+                 statement.setString(5, f.getId());
+          
+            
+
+                 int rowsInserted = statement.executeUpdate(); // Executa a inserção e retorna valor != 0 se inseriu (ID de inserção do banco)
+                 if (rowsInserted > 0) {
+                     System.out.println("Novo usuário inserido com sucesso");
+                 }
+                 statement.close();
+             }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     
+    public ArrayList listar(){
+        return null;
+}
 }
