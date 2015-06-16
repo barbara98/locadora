b27882b870a2cac7f93d8fe4d88e9f60499aa84e
info206 <info206@LAB02-15.edupit.intra>
2015-06-16 17:30:39 -0300

    barbara 4

--- src/Controller/AluguelController.java
+++ src/Controller/AluguelController.java
@@ -18,11 +18,13 @@ public class AluguelController {
        public void Cadastrar(Aluguel a) throws SQLException {
          Util util = new Util();
           try (Connection conexao = util.conecta()){
-         String sql ="INSERT INTO Aluguel (Nota_fiscal,Data_aluguel,Data_devolução)VALUES (?, ?, ?)";
+         String sql ="INSERT INTO Aluguel (Nota_fiscal,Data_aluguel,Data_devolução,idAluguel,Cliente_idCliente)VALUES (?, ?, ?,?,?)";
          PreparedStatement statement = conexao.prepareStatement(sql);
          statement.setString(1, a.getNota_fiscal());
          statement.setString(2, a.getData_aluguel());
          statement.setString(3, a.getData_devolução());
+      //Lembrar de pegar o id do aluguel(q não é auto incremento) e do cliente
+         statement.setInt(4, 2);
             
     
          int rowsInserted = statement.executeUpdate(); // Executa a inserção e retorna valor != 0 se inseriu (ID de inserção do banco)
